155b2ff2061f068e268b98ffea7506937cd3d073
Rod Vagg <rod@vagg.org>
2013-12-07 19:33:30 +1100

    added node 0.11+ versions

---
+++ 1_hello_world/node_0.12/hello.cc
@@ -0,0 +1,17 @@
+#include <node.h>
+#include <v8.h>
+
+using namespace v8;
+
+void Method(const v8::FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
+  args.GetReturnValue().Set(String::New("world"));
+}
+
+void Init(Handle<Object> exports) {
+  exports->Set(String::NewSymbol("hello"),
+      FunctionTemplate::New(Method)->GetFunction());
+}
+
+NODE_MODULE(hello, Init)
---
+++ 1_hello_world/node_0.12/hello.js
@@ -0,0 +1,3 @@
+var addon = require('./build/Release/hello');
+
+console.log(addon.hello()); // 'world'
