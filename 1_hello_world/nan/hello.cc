5d660dd7aa1466fa58df6582737d140c34b57d05
Rod Vagg <rod@vagg.org>
2013-12-07 19:33:20 +1100

    added nan versions

---
+++ 1_hello_world/nan/hello.cc
@@ -0,0 +1,15 @@
+#include <nan.h>
+
+using namespace v8;
+
+NAN_METHOD(Method) {
+  NanScope();
+  NanReturnValue(String::New("world"));
+}
+
+void Init(Handle<Object> exports) {
+  exports->Set(String::NewSymbol("hello"),
+      FunctionTemplate::New(Method)->GetFunction());
+}
+
+NODE_MODULE(hello, Init)
