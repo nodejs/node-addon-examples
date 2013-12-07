bd91f06dfb5c502d1309a443d63084082f08af18
Rod Vagg <rod@vagg.org>
2013-12-07 19:32:58 +1100

    move originals to node_0.10 subdir

--- 1_hello_world/node_0.10/hello.cc
+++ 1_hello_world/node_0.10/hello.cc
@@ -8,9 +8,9 @@ Handle<Value> Method(const Arguments& args) {
   return scope.Close(String::New("world"));
 }
 
-void init(Handle<Object> exports) {
+void Init(Handle<Object> exports) {
   exports->Set(String::NewSymbol("hello"),
       FunctionTemplate::New(Method)->GetFunction());
 }
 
-NODE_MODULE(hello, init)
+NODE_MODULE(hello, Init)
