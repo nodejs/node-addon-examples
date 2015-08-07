1c24155271d738a456cf9fa1c6825ed254aa1068
Roman Jurkov <winfinit@gmail.com>
2015-08-06 20:35:35 -0400

    wrapped method with nan, and changed argument type in init call

--- 3_callbacks/nan/addon.cc
+++ 3_callbacks/nan/addon.cc
@@ -1,14 +1,19 @@
 #include <nan.h>
 
-void RunCallback(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+NAN_METHOD(RunCallback) {
   v8::Local<v8::Function> cb = info[0].As<v8::Function>();
   const unsigned argc = 1;
-  v8::Local<v8::Value> argv[argc] = { Nan::New("hello world").ToLocalChecked() };
-  Nan::MakeCallback(Nan::GetCurrentContext()->Global(), cb, argc, argv);
+  v8::Local<v8::Value> argv[argc] = { Nan::New<v8::String>("hello world").ToLocalChecked() };
+  Nan::MakeCallback(info.This(), cb, argc, argv);
+
+  info.GetReturnValue().SetUndefined();
 }
 
-void Init(v8::Local<v8::Object> exports, v8::Local<v8::Object> module) {
-  Nan::SetMethod(module, "exports", RunCallback);
+void Init(v8::Handle<v8::Object> exports, v8::Handle<v8::Object> module) {
+  Nan::SetMethod(module
+    , "exports"
+    , RunCallback
+  );
 }
 
 NODE_MODULE(addon, Init)
