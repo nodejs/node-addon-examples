b3463585eb5f78cc1d170ee985dcf8c61bec3e5c
Roman Jurkov <winfinit@gmail.com>
2015-08-06 22:06:39 -0400

    added method wraps in 5th example

--- 5_function_factory/nan/addon.cc
+++ 5_function_factory/nan/addon.cc
@@ -1,20 +1,20 @@
 #include <nan.h>
 
-void MyFunction(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+NAN_METHOD(MyFunction) {
   info.GetReturnValue().Set(Nan::New("hello world").ToLocalChecked());
 }
 
-void CreateFunction(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+NAN_METHOD(CreateFunction) {
   v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(MyFunction);
   v8::Local<v8::Function> fn = tpl->GetFunction();
 
   // omit this to make it anonymous
-  fn->SetName(Nan::New("theFunction").ToLocalChecked());
+  fn->SetName(Nan::New<v8::String>("theFunction").ToLocalChecked());
 
   info.GetReturnValue().Set(fn);
 }
 
-void Init(v8::Local<v8::Object> exports, v8::Local<v8::Object> module) {
+void Init(v8::Handle<v8::Object> exports, v8::Handle<v8::Object> module) {
   Nan::SetMethod(module, "exports", CreateFunction);
 }
 
