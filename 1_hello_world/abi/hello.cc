f169334ce2b480900117d19d489e3733554c4fe0
Ian Halliday <ianhall@microsoft.com>
2016-06-14 14:56:51 -0700

    Add legacy:: namespace qualifier to fix build
    WorkaroundNewModuleInit API was moved to a new node::js::legacy
    namespace in the API.  Updated the current abi module examples to follow
    suit.

--- 1_hello_world/abi/hello.cc
+++ 1_hello_world/abi/hello.cc
@@ -19,7 +19,7 @@ void new_init(node::js::env env, node::js::value exports, node::js::value module
 // // For not I am working around the issue by assuming v8 and thunking into
 // // an init callback with a VM neutral signature.
 void Init(v8::Local<v8::Object> exports, v8::Local<v8::Object> module) {
-  node::js::WorkaroundNewModuleInit(exports, module, new_init);
+  node::js::legacy::WorkaroundNewModuleInit(exports, module, new_init);
 }
 
 NODE_MODULE(addon, Init)
