14abe5c637b98050f0d175efc66105517cbd7cc2
Gabriel Schulhof <gabriel.schulhof@intel.com>
2018-09-04 11:57:47 -0400

    Add async work thread-safe function example
    Add an example that calls into JavaScript from the `Execute` callback
    of a `napi_async_work` item by using a `napi_threadsafe_function`.

---
+++ async_work_thread_safe_function/node-api/index.js
@@ -0,0 +1,8 @@
+// Use the "bindings" package to locate the native bindings.
+const binding = require('bindings')('binding');
+
+// Call the function "startThread" which the native bindings library exposes.
+// The function accepts a callback which it will call from the worker thread and
+// into which it will pass prime numbers. This callback simply prints them out.
+binding.startThread((thePrime) =>
+  console.log("Received prime from secondary thread: " + thePrime));
