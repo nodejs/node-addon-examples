f39b350f844425d8a36b5c56849ab67d32068df6
Gabriel Schulhof <gabriel.schulhof@intel.com>
2019-03-31 11:25:23 -0700

    Add example passing an ArrayBuffer to a native function

---
+++ array_buffer_to_native/node-addon-api/array_buffer_to_native.cc
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <napi.h>
+
+static void ArrayConsumer(const int32_t* array, size_t length) {
+  for (size_t index = 0; index < length; index++) {
+    fprintf(stderr, "array[%lu] = %d\n", index, array[index]);
+  }
+}
+
+static Napi::Value AcceptArrayBuffer(const Napi::CallbackInfo& info) {
+  if (info.Length() != 1) {
+    Napi::Error::New(info.Env(), "Expected exactly one argument")
+      .ThrowAsJavaScriptException();
+    return info.Env().Undefined();
+  }
+  if (!info[0].IsArrayBuffer()) {
+    Napi::Error::New(info.Env(), "Expected an ArrayBuffer")
+      .ThrowAsJavaScriptException();
+    return info.Env().Undefined();
+  }
+
+  Napi::ArrayBuffer buf = info[0].As<Napi::ArrayBuffer>();
+
+  ArrayConsumer(reinterpret_cast<int32_t*>(buf.Data()),
+                buf.ByteLength()/sizeof(int32_t));
+
+  return info.Env().Undefined();
+}
+
+static Napi::Object Init(Napi::Env env, Napi::Object exports) {
+  exports["AcceptArrayBuffer"] = Napi::Function::New(env, AcceptArrayBuffer);
+  return exports;
+}
+
+NODE_API_MODULE(NODE_GYP_MODULE_NAME, Init)
---
+++ array_buffer_to_native/node-addon-api/index.js
@@ -0,0 +1,15 @@
+const binding = require('bindings')('array_buffer_to_native');
+const array = new Int32Array(10);
+
+array[0] = 19;
+array[1] = -41;
+array[2] = 98;
+array[3] = -922;
+array[4] = 587;
+array[5] = 12;
+array[6] = 221;
+array[7] = 49;
+array[8] = -96;
+array[9] = -1;
+
+binding.AcceptArrayBuffer(array.buffer);
