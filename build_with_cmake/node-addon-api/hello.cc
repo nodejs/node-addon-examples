768eb1e79fa2a4ed928e607cc83d9bcf3be632d4
Jim Schlight <jim@inspiredware.com>
2019-09-23 12:04:30 -0700

    Adds statics requested by Gabriel
    See https://github.com/nodejs/node-addon-examples/pull/113

--- build_with_cmake/node-addon-api/hello.cc
+++ build_with_cmake/node-addon-api/hello.cc
@@ -1,11 +1,11 @@
 #include <napi.h>
 
-Napi::String Method(const Napi::CallbackInfo& info) {
+static Napi::String Method(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
   return Napi::String::New(env, "Hello, world!");
 }
 
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
+static Napi::Object Init(Napi::Env env, Napi::Object exports) {
   exports.Set(Napi::String::New(env, "hello"),
               Napi::Function::New(env, Method));
   return exports;
