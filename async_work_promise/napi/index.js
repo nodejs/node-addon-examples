9b28d4881f8de0a348647400fe25c490f38a3d1d
legendecas <legendecas@gmail.com>
2020-02-12 12:36:24 +0800

    Add an example of combining async work and promises
    Fixes https://github.com/nodejs/node-addon-api/issues/634

---
+++ async_work_promise/napi/index.js
@@ -0,0 +1,10 @@
+// Use the "bindings" package to locate the native bindings.
+const binding = require('bindings')('binding');
+
+// Call the function "startWork" which the native bindings library exposes.
+// The function returns a promise which will be resolved at the complete of the
+// work with an array of worked out primes. This resolution simply prints them out.
+binding.startWork()
+  .then((thePrimes) => {
+    console.log("Received primes from completed work: " + thePrimes)
+  });
