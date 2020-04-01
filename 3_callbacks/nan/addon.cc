21f17646c7ad0d3edb1b5deac83049e06bfe8e36
zhangwinning <1527841714@qq.com>
2020-04-01 09:50:27 +0800

    using AsyncResource::runInAsyncScope invoke callback in the nan… (#132)

--- 3_callbacks/nan/addon.cc
+++ 3_callbacks/nan/addon.cc
@@ -4,7 +4,8 @@ void RunCallback(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   v8::Local<v8::Function> cb = info[0].As<v8::Function>();
   const unsigned argc = 1;
   v8::Local<v8::Value> argv[argc] = {Nan::New("hello world").ToLocalChecked()};
-  Nan::MakeCallback(Nan::GetCurrentContext()->Global(), cb, argc, argv);
+  Nan::AsyncResource resource("nan:makeCallback");
+  resource.runInAsyncScope(Nan::GetCurrentContext()->Global(), cb, argc, argv);
 }
 
 void Init(v8::Local<v8::Object> exports, v8::Local<v8::Object> module) {
