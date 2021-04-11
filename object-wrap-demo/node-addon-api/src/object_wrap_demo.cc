01839c8ac4c86566da0cb57a2d4cdc4d9721c4fa
zoomdong <1344492820@qq.com>
2021-04-11 23:16:09 +0800

    chore: remove more napi namespace usage

--- object-wrap-demo/node-addon-api/src/object_wrap_demo.cc
+++ object-wrap-demo/node-addon-api/src/object_wrap_demo.cc
@@ -4,47 +4,47 @@ using namespace Napi;
 
 ObjectWrapDemo::ObjectWrapDemo(const Napi::CallbackInfo& info)
     : ObjectWrap(info) {
-  Napi::Env env = info.Env();
+  Env env = info.Env();
 
   if (info.Length() < 1) {
-    Napi::TypeError::New(env, "Wrong number of arguments")
+    TypeError::New(env, "Wrong number of arguments")
         .ThrowAsJavaScriptException();
     return;
   }
 
   if (!info[0].IsString()) {
-    Napi::TypeError::New(env, "You need to name yourself")
+    TypeError::New(env, "You need to name yourself")
         .ThrowAsJavaScriptException();
     return;
   }
 
-  this->_greeterName = info[0].As<Napi::String>().Utf8Value();
+  this->_greeterName = info[0].As<String>().Utf8Value();
 }
 
-Napi::Value ObjectWrapDemo::Greet(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
+Value ObjectWrapDemo::Greet(const CallbackInfo& info) {
+  Env env = info.Env();
 
   if (info.Length() < 1) {
-    Napi::TypeError::New(env, "Wrong number of arguments")
+    TypeError::New(env, "Wrong number of arguments")
         .ThrowAsJavaScriptException();
     return env.Null();
   }
 
   if (!info[0].IsString()) {
-    Napi::TypeError::New(env, "You need to introduce yourself to greet")
+    TypeError::New(env, "You need to introduce yourself to greet")
         .ThrowAsJavaScriptException();
     return env.Null();
   }
 
-  Napi::String name = info[0].As<Napi::String>();
+  String name = info[0].As<String>();
 
   printf("Hello %s\n", name.Utf8Value().c_str());
   printf("I am %s\n", this->_greeterName.c_str());
 
-  return Napi::String::New(env, this->_greeterName);
+  return String::New(env, this->_greeterName);
 }
 
-Napi::Function ObjectWrapDemo::GetClass(Napi::Env env) {
+Function ObjectWrapDemo::GetClass(Env env) {
   return DefineClass(
       env,
       "ObjectWrapDemo",
@@ -53,8 +53,8 @@ Napi::Function ObjectWrapDemo::GetClass(Napi::Env env) {
       });
 }
 
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  Napi::String name = Napi::String::New(env, "ObjectWrapDemo");
+Object Init(Env env, Object exports) {
+  String name = String::New(env, "ObjectWrapDemo");
   exports.Set(name, ObjectWrapDemo::GetClass(env));
   return exports;
 }
