a94f7b6c51fcba8e48de2d581343f0fb0daa102d
Nate Brake <33383515+njbrake@users.noreply.github.com>
2021-10-14 08:41:41 -0400

    Add some comments
    Very basic comments to help explain what the Env variable is doing and how the memory handoff is working.

--- build_with_cmake/node-addon-api/hello.cc
+++ build_with_cmake/node-addon-api/hello.cc
@@ -1,8 +1,16 @@
 #include <napi.h>
 
+
 static Napi::String Method(const Napi::CallbackInfo& info) {
+  // Napi::Env is the opaque data structure containing the environment in which the request is being run. 
+  // We will need this env when we want to create any new objects inside of the node.js environment
   Napi::Env env = info.Env();
-  return Napi::String::New(env, "Hello, world!");
+  
+  // Create a C++ level variable
+  std::string helloWorld = "Hello, world!";
+  
+  // Return a new javascript string that we copy-construct inside of the node.js environment
+  return Napi::String::New(env, helloWorld);
 }
 
 static Napi::Object Init(Napi::Env env, Napi::Object exports) {
