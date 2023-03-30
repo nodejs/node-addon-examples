9a002b5d710f5adaa3ef7ed1d52cfbc28516ace7
Gabriel Schulhof <gabrielschulhof@gmail.com>
2023-03-30 10:42:13 -0700

    example: Add Addon<T> example

---
+++ 1_hello_world/node-addon-api-addon-class/hello.cc
@@ -0,0 +1,17 @@
+#include <napi.h>
+
+class HelloAddon : public Napi::Addon<HelloAddon> {
+ public:
+  HelloAddon(Napi::Env env, Napi::Object exports) {
+    DefineAddon(exports, {
+      InstanceMethod("hello", &HelloAddon::Hello, napi_enumerable)
+    });
+  }
+
+ private:
+  Napi::Value Hello(const Napi::CallbackInfo& info) {
+    return Napi::String::New(info.Env(), "world");
+  }
+};
+
+NODE_API_ADDON(HelloAddon)
