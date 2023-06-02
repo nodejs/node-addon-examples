e0a721bb4cae483969bfd5ba5f97b08b7ebc5269
Darshan Sen <raisinten@gmail.com>
2023-06-02 20:51:51 +0530

    example: add example for calling JS from AsyncWorker::Execute()
    This was asked for in
    https://github.com/nodejs/node-addon-api/issues/110#issuecomment-415536418
    and
    https://github.com/nodejs/node-addon-api/issues/110#issuecomment-462586274.
    The solution was described in text in
    https://github.com/nodejs/node-addon-api/issues/110#issuecomment-415580690
    but it would be better to have a code example that folks can refer to.
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

---
+++ call-js-from-async-worker-execute/node-addon-api/index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const { EventEmitter } = require('node:events');
+const { dispatch } = require('bindings')('dispatcher');
+
+class Socket extends EventEmitter {
+  constructor () {
+    super();
+    dispatch(this.emit.bind(this));
+  }
+}
+
+const socket = new Socket();
+
+socket.on('open', () => {
+  console.log('opened');
+});
+
+socket.on('message', (message) => {
+  console.log(`message: ${message}`);
+});
+
+socket.on('close', () => {
+  console.log('closed');
+});
