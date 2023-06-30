2fc37f6d85d8dc2a7a4e14dc42e66fa924d61b69
matthewkeil <me@matthewkeil.com>
2023-06-30 02:43:50 -0700

    refactor: add a bit of spacing for clarity

--- async_work_promise/node-addon-api/addon.cc
+++ async_work_promise/node-addon-api/addon.cc
@@ -3,18 +3,21 @@
 
 Napi::Value DoHeavyMath(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
+
   if (!info[0].IsNumber()) {
     Napi::TypeError::New(env, "num1 must be a number")
         .ThrowAsJavaScriptException();
     return env.Undefined();
   }
   uint32_t num_1 = info[0].As<Napi::Number>().Uint32Value();
+  
   if (!info[1].IsNumber()) {
     Napi::TypeError::New(env, "num2 must be a number")
         .ThrowAsJavaScriptException();
     return env.Undefined();
   }
   uint32_t num_2 = info[1].As<Napi::Number>().Uint32Value();
+  
   DoHeavyMathWorker* worker = new DoHeavyMathWorker(env, num_1, num_2);
   worker->Queue();
   return worker->GetPromise();
