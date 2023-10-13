fd71b7798085eb44808ce6b18b2a8fb3dd12765b
Matthew Keil <me@matthewkeil.com>
2023-10-13 11:28:57 +0200

    refactor: create directory structure to organize exmaples

--- build_with_cmake/napi/hello.js
+++
@@ -1,3 +0,0 @@
-var addon = require('bindings')('build-napi-with-cmake');
-
-console.log(addon.hello()); // 'world'
--- build_with_cmake/node-addon-api/hello.js
+++
@@ -1,3 +0,0 @@
-var addon = require('bindings')('build-node-addon-api-with-cmake');
-
-console.log(addon.hello()); // 'world'
