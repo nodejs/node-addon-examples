b7c497e54d165c349874246bf6fa9d462c0cd58b
urielch <uriel.chemouni@gmail.com>
2024-09-09 16:39:00 +0200

    add types

--- src/5-async-work/async_pi_estimate/type.d.ts
+++ src/5-async-work/async_pi_estimate/type.d.ts
@@ -1,2 +1,2 @@
 export function calculateSync(num: number): number;
-export function calculateAsync(num: number, callback:(num: string) => void): void;
+export function calculateAsync(num: number, callback:(err: Error, num: string) => void): void;
