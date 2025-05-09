d2c7a95bbcd102ca32ee80c86e1a198c6797cc9f
Rich Lowe <26016332+om-mani-padme-hum@users.noreply.github.com>
2025-05-09 16:20:40 +0000

    Remove unnecessary object reference

--- src/1-getting-started/8_multiple_object_exports/addon.cc
+++ src/1-getting-started/8_multiple_object_exports/addon.cc
@@ -2,12 +2,6 @@
 #include "otherobject-napi.h"
 
 Napi::Object init(Napi::Env env, Napi::Object exports) {
-  Napi::ObjectReference *data = new Napi::ObjectReference();
-  
-  *data = Napi::Reference<Napi::Object>::New(Napi::Object::New(env), 1);
-  
-  env.SetInstanceData(data);
-    
   MyObjectNapi::init(env, exports);
   OtherObjectNapi::init(env, exports);
   
